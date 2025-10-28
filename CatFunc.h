7f07ab77a2afb0e2499c5698e237646732348ec8
Cavan Ray Theiss <cavanrt@gmail.com>
2025-10-28 07:08:44 -0500

    moves function declarations to CatFunc.h

--- CatFunc.h
+++ CatFunc.h
@@ -1,2 +1,24 @@
 #pragma once
 
+
+
+//Function declarations for program structure, defined after main function
+
+
+void saveCatalogue();
+
+void loadCatalogue();
+
+void optionsMenu(car*& head, std::vector<car*>& carCatalogue);
+
+void printCatalogue(car* n);
+
+void printCar(car* n);
+
+car* findCar(car*& head);
+
+void addCar(car*& head, std::vector<car*>& carCatalogue);
+
+void editCar(car* n);
+
+void deleteCar(car*& head, std::vector<car*>& carCatalogue, car* n);
