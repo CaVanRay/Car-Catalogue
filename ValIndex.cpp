413e846281e7c939e12c7f1689146705f40bb64e
Cavan Theiss <cavanrt@gmail.com>
2025-11-11 19:07:05 -0600

    Update ValIndex.cpp

--- ValIndex.cpp
+++ ValIndex.cpp
@@ -1,1 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cctype>
+#include "CatFunc.h"
+#include "Cars.h"
+#include "Savld.h"
 
+//**********************************************************************************************************************
+
+/*
+optionsMenu function: Displays a menu of options to the user and prompts them to select an action.
+
+STATUS: TESTING
+*/
