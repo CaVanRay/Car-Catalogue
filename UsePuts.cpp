9fbfe275f14f341fb3fa5c3dca80db98c81f415a
Cavan Theiss <cavanrt@gmail.com>
2025-11-13 19:45:00 -0600

    Update UsePuts.cpp

--- UsePuts.cpp
+++ UsePuts.cpp
@@ -103,10 +103,28 @@ void printCatalogue(){
 
 //**********************************************************************************************************************
 
-void printCar(){
+/*
+printCar function: Takes a pointer to a car object and prints all of its details to the console.
 
+STATUS: WORKING
+*/
 
-  
+void printCar(car* n) {
+    std::cout << std::endl << "***************************************" << std::endl;
+    std::cout << "Owner: " << n->GetOwner() << std::endl;
+    std::cout << "Model: " << n->GetModel() << std::endl;
+    std::cout << "VIN# : " << n->GetVin() << std::endl;
+    std::cout << "Miles: " << n->GetMileage() << std::endl;
+    std::cout << "MPG  : " << n->GetMPG() << std::endl;
+    std::cout << "Price: " << n->GetPrice() << std::endl;
+    std::cout << "Status: ";
+    if (n->GetSale()) {
+        std::cout << "Still available" << std::endl;
+    }
+    else {
+        std::cout << "Not available" << std::endl;
+    }
+
 }
 
 //**********************************************************************************************************************
