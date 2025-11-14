430d4441099eb334313d548a015d684784cf9f42
Cavan Theiss <cavanrt@gmail.com>
2025-11-13 19:45:27 -0600

    Update UsePuts.cpp

--- UsePuts.cpp
+++ UsePuts.cpp
@@ -95,10 +95,37 @@ void optionsMenu(car*& head, std::vector<car*>& carCatalogue) {
 
 //**********************************************************************************************************************
 
-void printCatalogue(){
+/*
+printCatalogue function: Takes a pointer to the head of the linked list of car objects and iterates through the list,
 
+STATUS: WORKING
+*/
 
-  
+void printCatalogue(car* n) {
+
+    if (n == nullptr) {
+        std::cout << std::endl << "No cars are currently listed in catalogue." << std::endl;
+    }
+
+    while (n != nullptr) {
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << "Index#: " << n->GetIndex() << std::endl;
+        std::cout << "Owner: " << n->GetOwner() << std::endl;
+        std::cout << "Model: " << n->GetModel() << std::endl;
+        std::cout << "VIN# : " << n->GetVin() << std::endl;
+        std::cout << "Miles: " << n->GetMileage() << std::endl;
+        std::cout << "MPG  : " << n->GetMPG() << std::endl;
+        std::cout << "Price: $" << n->GetPrice() << std::endl;
+        std::cout << "Status: ";
+        if (n->GetSale()) {
+            std::cout << "Still available" << std::endl;
+        }
+        else {
+            std::cout << "Not available" << std::endl;
+        }
+        n = n->GetNext();
+    }
+    std::cout << std::endl;
 }
 
 //**********************************************************************************************************************
