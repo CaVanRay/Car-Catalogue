f5f430aed43dc437e931eef5ae40269a0c05afbb
Cavan Theiss <cavanrt@gmail.com>
2025-11-13 19:39:45 -0600

    Update UsePuts.cpp

--- UsePuts.cpp
+++ UsePuts.cpp
@@ -1,1 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cctype>
+#include "CatFunc.h"
+#include "Cars.h"
+#include "Savld.h"
 
+//**********************************************************************************************************************
