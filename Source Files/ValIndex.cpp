cbc186ae32b2a6b11093cdfcd21341452386d007
Cavan Theiss <cavanrt@gmail.com>
2025-11-15 17:36:58 -0600

    Update ValIndex.cpp

--- Source Files/ValIndex.cpp
+++ Source Files/ValIndex.cpp
@@ -19,7 +19,26 @@ void indexUpdate(car*& head, std::vector<car*>& carCatalogue){
       if (n == nullptr) {
           std::cout << std::endl << "No cars are currently listed in catalogue." << std::endl;
       }
-
+      while (n != nullptr) {
+              std::cout << std::endl << "***************************************" << std::endl;
+              std::cout << "Index#: " << n->GetIndex() << std::endl;
+              std::cout << "Owner: " << n->GetOwner() << std::endl;
+              std::cout << "Model: " << n->GetModel() << std::endl;
+              std::cout << "VIN# : " << n->GetVin() << std::endl;
+              std::cout << "Miles: " << n->GetMileage() << std::endl;
+              std::cout << "MPG  : " << n->GetMPG() << std::endl;
+              std::cout << "Price: $" << n->GetPrice() << std::endl;
+              std::cout << "Status: ";
+              if (n->GetSale()) {
+                  std::cout << "Still available" << std::endl;
+              }
+              else {
+                  std::cout << "Not available" << std::endl;
+              }
+              n = n->GetNext();
+          }
+          std::cout << std::endl;
+      }
   
 }
 
