561c5c9fbdb3767599468bd02023a781e029e178
Cavan Theiss <cavanrt@gmail.com>
2025-11-30 17:55:11 -0600

    Update CatFunc.h

--- CatFunc.h
+++ CatFunc.h
@@ -3,16 +3,6 @@
 #include "Cars.h"
 
 //Function declarations for program structure, defined after main function
-
-/*
-void optionsMenu(car*& head, std::vector<car*>& carCatalogue);
-
-void printCatalogue(car* n);
-
-void printCar(car* n);
-
-car* findCar(car*& head);
-*/
   
 void addCar(car*& head, std::vector<car*>& carCatalogue);
 
