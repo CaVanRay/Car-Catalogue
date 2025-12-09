25615e9e862693e394ff83faa30108a0ac257f83
Cavan Theiss <cavanrt@gmail.com>
2025-12-09 15:59:33 -0600

    Update CatFunc.cpp

--- CatFunc.cpp
+++ CatFunc.cpp
@@ -253,7 +253,8 @@ void moveCar(car*& head, std::vector<car*>& carCatalogue, car*n){
 		head = n;	
 	}
 	else{
-
+		// Every spot other than the 1st position
+		// 
 		car* PrevNode = carCatalogue[newindex - 2];
 		car* NextNode = (newindex - 1 < carCatalogue.size()) ? carCatalogue[newindex - 1] : nullptr;
 
