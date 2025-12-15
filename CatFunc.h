0711848d7cce139f5b0b0ab17501cbe52c05977a
Cavan Theiss <cavanrt@gmail.com>
2025-12-15 13:28:58 -0600

    Update CatFunc.h

--- CatFunc.h
+++ CatFunc.h
@@ -10,7 +10,9 @@ void addCar(car*& head, std::vector<car*>& carCatalogue);
 // repCar() : swaps out existing car for a new car
 void repCar(car* n);
 
+// editCar() : changes selected value on an existing car
 void editCar(car* n);
+
 
 void deleteCar(car*& head, car*& tail, std::vector<car*>& carCatalogue, car* n);
 
