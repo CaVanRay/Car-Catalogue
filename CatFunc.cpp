3c46a8a9228efe4975f35edd02c57a90d18179ec
Cavan Theiss <cavanrt@gmail.com>
2025-12-20 15:38:37 -0600

    Update CatFunc.cpp

--- CatFunc.cpp
+++ CatFunc.cpp
@@ -139,7 +139,7 @@ void editCar(car* n) {
             break;
         }
         case 3: {
-            std::string newVin = getVIN(head, "Enter VIN: ");
+            std::string newVin = getVIN("Enter VIN: ", head);
             n->SetVin(newVin);
             break;
         }
