dc7d2e897d4a3abd66132dcb02507f413b30652a
CaVanRay <cavanrt@gmail.com>
2026-05-23 20:10:43 -0500

    adding src file

---
+++ src/CatFunc.cpp
@@ -0,0 +1,319 @@
+#include "CarCatHeaders.h"
+
+//**********************************************************************************************************************
+
+/*
+addCar function: Collects user input to create a new car object and adds it to the end of the linked list.
+If the list is empty, the new car becomes the head of the list.
+
+STATUS: WORKING
+*/
+
+void addCar(car*& head, std::vector<car*>& carCatalogue) {
+
+    bool Cont = true;
+    do {
+
+        std::cout << std::endl << "Please Add a vehicle to the catalogue" << std::endl << std::endl;
+
+        int index;
+        std::string Owner = getString("input car owner: ");
+        std::string Model = getString("input car model: ");
+        std::string Vin = getVIN("input car vin#: ", head);
+        int Mileage = getInt("input car mileage: ");
+        double MPG = getDouble("input car MPG: ");
+        double Price = getDouble("input car price: ");
+        bool ForSale = getYN("is car available for purchase? (Y/N): ");
+
+        //***********************************************************************
+
+        car* newCar = new car();
+        newCar->SetOwner(Owner);
+        newCar->SetModel(Model);
+        newCar->SetVin(Vin);
+        newCar->SetMileage(Mileage);
+        newCar->SetMPG(MPG);
+        newCar->SetPrice(Price);
+        newCar->SetSale(ForSale);
+
+
+        if (!carCatalogue.empty()) {
+            newCar->SetPrev(carCatalogue.back());
+            carCatalogue.back()->SetNext(newCar);
+			tail = newCar;
+            newCar->SetIndex(carCatalogue.size() + 1);
+        }
+        else {
+            head = newCar;
+			tail = newCar;
+            newCar->SetIndex(1);
+        }
+
+        carCatalogue.push_back(newCar);
+
+        system("CLS");
+
+        Cont = getYN("Would you like to add another car? (Y/N): ");
+		
+    } while (Cont);
+
+	// Update the index's of each item in linked list
+	// Update the carCatalogue vector with linked list
+	indexUpdate(head, carCatalogue);
+
+	system("CLS");
+}
+
+
+//**********************************************************************************************************************
+
+/*
+repCar function: User chooses a car already in the catalogue and then enters all new information, effectivly swapping one car out for another
+
+STATUS: WRITING
+*/
+
+void repCar(car*& head, car* n) {
+
+std::string Owner = getString("input car owner: ");
+std::string Model = getString("input car model: ");
+std::string Vin = getVIN("input car vin#: ", head);
+int Mileage = getInt("input car mileage: ");
+double MPG = getDouble("input car MPG: ");
+double Price = getDouble("input car price: ");
+bool ForSale = getYN("is car available for purchase? (Y/N): ");
+
+ n->SetOwner(Owner);
+ n->SetModel(Model);
+ n->SetVin(Vin);
+ n->SetMileage(Mileage);
+ n->SetMPG(MPG);
+ n->SetPrice(Price);
+ n->SetSale(ForSale);
+}
+
+//**********************************************************************************************************************
+
+/*
+editCar function: Takes a pointer to a car object and allows the user to edit any of its attributes by selecting from a menu.
+
+STATUS: TESTING
+*/
+
+void editCar(car*& head, car* n) {
+
+    bool Cont = true;
+    do {
+        // system("CLS");
+        printCar(n);
+        int choice;
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << std::endl << "1) - Owner";
+        std::cout << std::endl << "2) - Model";
+        std::cout << std::endl << "3) - Vin";
+        std::cout << std::endl << "4) - Mileage";
+        std::cout << std::endl << "5) - MPG";
+        std::cout << std::endl << "6) - Price";
+        std::cout << std::endl << "7) - Availability";
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << "Please enter number for desired selection: ";
+        while (!(std::cin >> choice)) {
+            std::cout << "Invalid input. Please enter a valid number: ";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        switch (choice) {
+        case 1: {
+            std::string newOwner;
+            std::cout << "Enter new owner: ";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::getline(std::cin, newOwner);
+            n->SetOwner(newOwner);
+            break;
+        }
+        case 2: {
+            std::string newModel;
+            std::cout << "Enter new model: ";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::getline(std::cin, newModel);
+            n->SetModel(newModel);
+            break;
+        }
+        case 3: {
+            std::string newVin = getVIN("Enter VIN: ", head);
+            n->SetVin(newVin);
+            break;
+        }
+        case 4: {
+            int newMileage = getInt("Enter new mileage: ");
+            n->SetMileage(newMileage);
+            break;
+        }
+        case 5: {
+            double newMPG = getDouble("Enter new MPG: ");
+            n->SetMPG(newMPG);
+            break;
+        }
+        case 6: {
+            double newPrice = getDouble("Enter new price: ");
+            n->SetPrice(newPrice);
+            break;
+        }
+        case 7: {
+            std::cout << "Is the car available for purchase? \n";
+            bool newForSale = getYN("Enter (Y/N): ");
+            n->SetSale(newForSale);
+            break;
+        }
+        default: {
+            std::cout << std::endl << "you have not selected a valid option" << std::endl;
+            break;
+        }
+        }
+        Cont = getYN("Would you like to change anything else? (Y/N): ");
+		
+    } while (Cont);
+}
+
+//**********************************************************************************************************************
+
+/*
+deleteCar function: Takes a pointer to a car object and removes it from the linked list by updating the previous and next pointers
+
+STATUS: TESTING
+*/
+// I think if I delete the first car in the catalogue, that it will break several functions that require head
+// Testing this now
+
+void deleteCar(car*& head, car*& tail, std::vector<car*>& carCatalogue, car* n) {
+
+    if (n == nullptr) return;
+
+    // If this is the head
+    if (n->GetPrev() == nullptr) {
+		if (n->GetNext()){
+        	head = n->GetNext(); // update head
+			head->SetPrev(nullptr);
+		} else {
+			head = nullptr;
+			tail = nullptr;
+		}
+    }
+    else if (n->GetNext() == nullptr) { // if this is the tail
+		if (n->GetPrev()) { 
+			tail = n->GetPrev();
+			tail->SetNext(nullptr);
+		} else {
+		head = nullptr;
+        tail = nullptr;
+		}
+    }
+    else { // middle of the list
+        n->GetPrev()->SetNext(n->GetNext());
+        n->GetNext()->SetPrev(n->GetPrev());
+    }
+    delete n;
+	indexUpdate(head, carCatalogue);
+    std::cout << std::endl << "Car deleted successfully." << std::endl;
+}
+
+//**********************************************************************************************************************
+
+// moveCar() function: Selected car is moved up or down the linked list to a desired location
+// STATUS: WORKING (UN-TESTED)
+
+void moveCar(car*& head, car*& tail, std::vector<car*>& carCatalogue, car*n){
+	/* 
+	This will be the moveCar() function
+	Pretty self-explanitory, it moves cars up and down the list
+	*/
+
+	// get current catalogue size
+	car* Pcount = head;
+    int listSize = 0;
+    while (Pcount != nullptr) {
+            listSize++;
+            Pcount = Pcount->GetNext();
+    }
+
+	// get desired index #
+	int newindex = getInt("Enter desired new index for car: ");
+	
+		while (newindex < 1 || newindex > listSize || newindex == n->GetIndex()) {
+			if(newindex < 1 || newindex > listSize){
+				newindex = getInt("Invalid index, Try again: ");
+			}else{
+				newindex = getInt("requested index is current index, Try again: ");
+			}
+		}
+		
+	std::cout << std::endl << std::endl;
+	std::cout << "Desired index: " << newindex << std::endl;
+	std::cout << "Current index: " << n->GetIndex() << std::endl;
+
+	// First we need to remove n from the list, but also make sure neither the prev or next are nullptr's
+
+	if((n->GetPrev() != nullptr) && (n->GetNext() != nullptr)) {
+		// Case 1: n is in the middle
+		// Both Prev Car & Next Car exist
+		// Attach Prev Car & Next Car to each other instead of n
+		n->GetPrev()->SetNext(n->GetNext());
+		n->GetNext()->SetPrev(n->GetPrev());
+		
+	}else if((n->GetPrev() != nullptr) && (n->GetNext() == nullptr)){
+		// Case 2: n is the tail
+		// Prev Car Exists but Next Car does not
+		// Set Prev Car's Next pointer to NULL
+		n->GetPrev()->SetNext(nullptr);
+		tail = n->GetPrev();
+		
+	}else if((n->GetPrev() == nullptr) && (n->GetNext() != nullptr)){
+		// Case 3: n is the head
+		// Next Car Exists but Prev Car does not
+		// Set Next Car's Prev pointer to NULL
+		n->GetNext()->SetPrev(nullptr);
+		head = n->GetNext();
+	}
+
+	// Set n's Next pointer to NULL
+	// Set n's Prev pointer to NULL
+	n->SetNext(nullptr);
+	n->SetPrev(nullptr);
+
+	// Update the carCatalogue index
+	indexUpdate(head, carCatalogue);
+
+	// If desired index is in the first slot
+	// set n's next pointer to previous head
+	// set previous head's Prev pointer to equal n
+	// set head pointer to equal n
+	if(newindex == 1){
+		n->SetNext(head);
+		// Make sure head currently exists
+		// If it does, set head's prev pointer to n
+		if (head) head->SetPrev(n);
+		head = n;	
+	}
+	else{
+		// For every spot other than the 1st position
+		// Using the requested index and the CarCatalogue vector list of car* pointers
+		// the index is already off by 1, so to set Prev pointer I subtract 2
+		car* PrevNode = carCatalogue[newindex - 2];
+		// the item currently in the requested position will be bumped over and will be used to set Next pointer 
+		car* NextNode = (newindex - 1 < carCatalogue.size()) ? carCatalogue[newindex - 1] : nullptr;
+
+		PrevNode->SetNext(n);
+		n->SetPrev(PrevNode);
+
+		if (NextNode != nullptr){
+			NextNode->SetPrev(n);
+			n->SetNext(NextNode);
+		} else {
+			tail = n;
+		}
+	}
+	
+		indexUpdate(head, carCatalogue);
+}
+
+//**********************************************************************************************************************
---
+++ src/UsePuts.cpp
@@ -0,0 +1,211 @@
+#include "CarCatHeaders.h"
+
+//**********************************************************************************************************************
+
+/*
+optionsMenu function: Displays a menu of options to the user and prompts them to select an action.
+
+STATUS: TESTING
+*/
+
+void optionsMenu(car*& head, car*& tail, std::vector<car*>& carCatalogue) {
+
+    bool runP = true;
+
+    do {
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << "1) Print Catalogue" << std::endl;
+        std::cout << "2) Print Specific Car" << std::endl;
+        std::cout << "3) Add a Car" << std::endl;
+		std::cout << "4) Replace a Car" << std::endl;
+        std::cout << "5) edit a Car" << std::endl;
+		std::cout << "6) Move a car" << std::endl;
+        std::cout << "7) Remove a Car" << std::endl;
+        std::cout << "8) Save Catalogue" << std::endl;
+        std::cout << "9) Exit Program" << std::endl;
+        std::cout << "***************************************" << std::endl;
+        int selectedAction = getInt("Enter option: ");
+        while (selectedAction < 1 || selectedAction > 9) {
+            selectedAction = getInt("Invalid option. Try again: ");
+        }
+        switch (selectedAction) {
+        case 1: {
+            system("CLS");
+            printCatalogue(head);
+            break;
+		}
+        case 2: {
+            system("CLS");
+            car* n = findCar(head);
+            printCar(n);
+            break;
+        }
+        case 3: {
+            system("CLS");
+            addCar(head, carCatalogue);
+            printCatalogue(head);
+            break;
+        }
+		case 4: {
+			system("CLS");
+			car* n = findCar(head);
+			repCar(head, n);
+			break;
+		}
+        case 5: {
+            system("CLS");
+            car* n = findCar(head);
+            editCar(head, n);
+            printCar(n);
+            break;
+        }
+		case 6: {
+			system("CLS");
+			car* n = findCar(head);
+			moveCar(head, tail, carCatalogue, n);
+			printCatalogue(head);
+			break;
+		}	
+        case 7: {
+            system("CLS");
+            car* n = findCar(head);
+            deleteCar(head, tail, carCatalogue, n);
+            printCatalogue(head);
+            break;
+        }
+        case 8: {
+            system("CLS");
+            saveCatalogue(head);
+            break;
+        }
+        case 9: {
+			std::cout << std::endl;
+			if(getYN("Would you like to save before you exit? (Y/N): ")){
+				system("CLS");
+				saveCatalogue(head);
+			}
+            system("CLS");
+			runP = false;
+            break;
+        }
+        default: {
+            std::cout << std::endl << "Error: you have not selected a valid option" << std::endl;
+            break;
+        }
+
+        }
+	} while (runP);
+}
+
+
+//**********************************************************************************************************************
+
+/*
+printCatalogue function: Takes a pointer to the head of the linked list of car objects and iterates through the list,
+
+STATUS: WORKING
+*/
+
+void printCatalogue(car* n) {
+
+    if (n == nullptr) {
+        std::cout << std::endl << "No cars are currently listed in catalogue." << std::endl;
+    }
+
+    while (n != nullptr) {
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << "Index#: " << n->GetIndex() << std::endl;
+        std::cout << "Owner: " << n->GetOwner() << std::endl;
+        std::cout << "Model: " << n->GetModel() << std::endl;
+        std::cout << "VIN# : " << n->GetVin() << std::endl;
+        std::cout << "Miles: " << n->GetMileage() << std::endl;
+        std::cout << "MPG  : " << n->GetMPG() << std::endl;
+        std::cout << "Price: $" << n->GetPrice() << std::endl;
+        std::cout << "Status: ";
+        if (n->GetSale()) {
+            std::cout << "Still available" << std::endl;
+        }
+        else {
+            std::cout << "Not available" << std::endl;
+        }
+        n = n->GetNext();
+    }
+    std::cout << std::endl;
+}
+
+//**********************************************************************************************************************
+
+/*
+printCar function: Takes a pointer to a car object and prints all of its details to the console.
+
+STATUS: WORKING
+*/
+
+void printCar(car* n) {
+    std::cout << std::endl << "***************************************" << std::endl;
+    std::cout << "Owner: " << n->GetOwner() << std::endl;
+    std::cout << "Model: " << n->GetModel() << std::endl;
+    std::cout << "VIN# : " << n->GetVin() << std::endl;
+    std::cout << "Miles: " << n->GetMileage() << std::endl;
+    std::cout << "MPG  : " << n->GetMPG() << std::endl;
+    std::cout << "Price: " << n->GetPrice() << std::endl;
+    std::cout << "Status: ";
+    if (n->GetSale()) {
+        std::cout << "Still available" << std::endl;
+    }
+    else {
+        std::cout << "Not available" << std::endl;
+    }
+
+}
+
+//**********************************************************************************************************************
+
+/*
+findCar function: Takes a pointer to the head of the linked list of car objects and prompts the user to enter a valid car index.
+It then iterates through the linked list to find the car with the matching index and returns a pointer to that car object.
+
+STATUS: WORKING
+*/
+
+car* findCar(car*& head) {
+    car* Pfound = nullptr;
+
+
+    car* Pcount = head;
+    int listSize = 0;
+    while (Pcount != nullptr) {
+            listSize++;
+            Pcount = Pcount->GetNext();
+    }
+
+
+    if (head == nullptr) {
+        std::cout << "\n\n Error: no searchable list provided \n\n";
+        return nullptr;
+    }
+
+    do {
+        std::cout << "\n***************************************\n";
+        int indexSearch = getInt("Enter index of car to change: ");
+        while (indexSearch < 1 || indexSearch > listSize) {
+            indexSearch = getInt("Invalid index. Try again: ");
+        }
+
+        car* n = head;
+        while (n != nullptr) {
+            if (n->GetIndex() == indexSearch) {
+                Pfound = n;
+                break;
+            }
+            n = n->GetNext();
+        }
+
+        if (Pfound == nullptr)
+            std::cout << "Car not found, choose another index.\n";
+
+    } while (Pfound == nullptr);
+
+    return Pfound;
+}
+//**********************************************************************************************************************
