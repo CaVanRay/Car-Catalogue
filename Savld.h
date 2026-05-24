9d50931d468a91d50cd8626ffcee9418c70d7635
CaVanRay <cavanrt@gmail.com>
2026-05-23 20:15:19 -0500

    cleanup of all the files that where moved

--- Savld.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include "Cars.h"
-
-//Function declarations for program structure, defined after main function
-
-void saveCatalogue(car*& head);
-
-void loadCatalogue(car*& head, car*& tail, std::vector<car*>& carCatalogue);
