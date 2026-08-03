efac138bc41298375d0d19f92181caf802d15c8a
Cavan Theiss <cavanrt@gmail.com>
2026-08-03 15:05:41 -0500

    Update guiExample.cpp

--- src/guiExample.cpp
+++ src/guiExample.cpp
@@ -3,6 +3,8 @@ Title: GUI Example / test file
 Author: Cavan Ray Theiss
 
 Description: I'm starting with a bare bones gui written in c++
-using the QT library
+using the QT library and then messing with that to both learn
+more about writing gui programs and eventually come up with 
+something that works for my car-catalogue program
 
 */
